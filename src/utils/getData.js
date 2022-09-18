a7c8c5913948f4616f779048371c6e83f39ee146
Nazareno <nachi10@hotmail.com>
2022-09-18 13:15:21 -0300

    aerolab

---
+++ src/utils/getData.js
@@ -0,0 +1,19 @@
+const token = 'Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzI3MzIzZThkYTUyNTAwMjFhOTdhNTAiLCJpYXQiOjE2NjM1MTMxNTB9.D3ULqax4ERvKpUP01q_Fq1J7NvffwivofsrJtTcCR6I';
+const API = `https://coding-challenge-api.aerolab.co/products?token=${token}`;
+
+const getData = async (id) => {
+    const apiURL = id ? `${API}${id}` : API;
+
+    try 
+    {
+        const response = await fetch(apiURL);
+        const data = response.json();
+        return data;
+    } catch(error) 
+    {
+        console.log('Fetch Error..!!', error);
+    };
+    
+};
+
+export default getData;
